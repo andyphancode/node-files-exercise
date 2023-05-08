240af8c270a278548c4d8b42a1b8a43181e9a285
andyphancode <andyphanpxh@gmail.com>
2023-05-07 20:33:30 -0700

    finished assignment

---
+++ step2.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+
+function cat(path) {
+    fs.readFile(path, 'utf8', function(err, data) {
+        if (err) {
+            console.error(`Error reading ${path} : ${err}`);
+            process.exit(1);
+        }
+        else {
+            console.log(data);
+        }
+    })
+}
+
+
+async function webCat(url) {
+    try {
+        let resp = await axios.get(url);
+        console.log(resp.data);
+    }
+    catch (err){
+        console.error(`Error reading ${url} : ${err}`);
+        process.exit(1);
+    }
+}
+
+let path = process.argv[2]
+
+if (path.includes('http')) {
+    webCat(path);
+} else {
+    cat(path);
+}
